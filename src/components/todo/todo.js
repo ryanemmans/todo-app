06efaf5eb455daeeba31923bc1a93868b869389f
Ryan Emmans <ryanemmans@gmail.com>
2021-12-02 02:48:20 -0800

    Adds blueprint library, begins refactoring and creating components, begins styling.

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import useForm from '../../hooks/form.js';
+// import Header from '../header/Header.js';
+import './todo.scss';
+import { Label, Button } from '@blueprintjs/core';
 
 import { v4 as uuid } from 'uuid';
 
@@ -17,21 +20,20 @@ const ToDo = () => {
   }
 
   function deleteItem(id) {
-    const items = list.filter( item => item.id !== id );
+    const items = list.filter(item => item.id !== id);
     setList(items);
   }
 
   function toggleComplete(id) {
 
-    const items = list.map( item => {
-      if ( item.id == id ) {
-        item.complete = ! item.complete;
+    const items = list.map(item => {
+      if (item.id === id) {
+        item.complete = !item.complete;
       }
       return item;
     });
 
     setList(items);
-
   }
 
   useEffect(() => {
@@ -43,43 +45,45 @@ const ToDo = () => {
   return (
     <>
       <header>
-        <h1>To Do List: {incomplete} items pending</h1>
+        <h1 id="title">To Do List: {incomplete} items pending</h1>
       </header>
 
-      <form onSubmit={handleSubmit}>
+      <section className="todo">
+        <form className="form" onSubmit={handleSubmit}>
+          <h2>Add To Do Item</h2>
 
-        <h2>Add To Do Item</h2>
+          <Label>
+            <span>To Do Item </span>
+            <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
+          </Label>
 
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
+          <Label>
+            <span>Assigned To </span>
+            <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
+          </Label>
 
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
+          <Label>
+            <span>Difficulty </span>
+            <input onChange={handleChange} defaultValue={3} type="range" min={1} max={5} name="difficulty" />
+          </Label>
 
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={3} type="range" min={1} max={5} name="difficulty" />
-        </label>
+          <Label>
+            <Button icon="add" large="large" intent="primary" type="submit" text="Add Item" />
+          </Label>
+        </form>
 
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
-      </form>
+        {list.map(item => (
 
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
+          <div className="list" key={item.id}>
+            <p>{item.text}</p>
+            <p><small>Assigned to: {item.assignee}</small></p>
+            <p><small>Difficulty: {item.difficulty}</small></p>
+            <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
+            <hr />
+          </div>
+        ))}
 
+      </section>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
-import App from './app.js';
+import App from './App.js';
 
 class Main extends React.Component {
   render() {
