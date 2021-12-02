58746064a7b17579524dd0a93c5ffdd0c180b514
Ryan Emmans <ryanemmans@gmail.com>
2021-12-02 03:08:32 -0800

    Updates index.js.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
-import App from './App.js';
+import App from './App';
 
 class Main extends React.Component {
   render() {
